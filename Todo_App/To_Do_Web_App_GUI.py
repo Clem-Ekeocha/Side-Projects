b3a79f605a32b89c42f69e964b3f0d52047381e7
AccessClem <63481051+Clem-Ekeocha@users.noreply.github.com>
2023-12-28 02:53:15 +0000

    Implemented "Add new todo" feature

--- Todo_App/To_Do_Web_App_GUI.py
+++ Todo_App/To_Do_Web_App_GUI.py
@@ -3,6 +3,11 @@ import streamlit as st
 import Function_file as ff
 
 todos = ff.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    ff.write_todos(todos)
+
 
 st.title("Clement's Todo App")
 st. subheader("This is my Todo App")
@@ -13,7 +18,8 @@ for i, todo in enumerate(todos):
     # Use the index as a unique identifier for each checkbox
     st.checkbox(f"{i + 1}. {todo}")
 
-st.text_input(label="Enter a Todo Below", placeholder="Add new todo..")
+st.text_input(label="Enter a Todo Below", placeholder="Add new todo..",
+              on_change=add_todo, key="new_todo")
 
 
 
